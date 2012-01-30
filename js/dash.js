3047239cbdea4fd10e5d291faea40b095d368f3e
Jan Lindblom <lindblom.jan@gmail.com>
2012-01-30 20:03:57 +0100

    Messing around some more.

--- js/dash.js
+++ js/dash.js
@@ -1,1 +1,16 @@
 /* simply-dash JS functions. */
+
+function getVPHeight() {
+    return window.getSize().y;
+}
+
+function sizeBlocks() {
+    var blockHeight = getVPHeight() / 2;
+
+    $('block1').tween('height', blockHeight);
+    $('block2').tween('height', blockHeight);
+    $('block3').tween('height', blockHeight);
+    $('block4').tween('height', blockHeight);
+    $('block5').tween('height', blockHeight);
+    $('block6').tween('height', blockHeight);
+}
