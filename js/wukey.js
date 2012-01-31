59adec384f3d08c363535605db563f259e556c86
Jan Lindblom <lindblom.jan@gmail.com>
2012-01-31 21:59:00 +0100

    Fiddling.

--- js/wukey.js
+++ js/wukey.js
@@ -1,5 +1,9 @@
 /* This is where the Weather Underground API key goes. */
 
+/* See the documentation for more information:
+ * http://www.wunderground.com/weather/api/d/documentation.html
+ */
+
 var myAPIkey = '';
 
 function getKey() {
